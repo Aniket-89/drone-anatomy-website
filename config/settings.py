bf7a1e5b6d3143c1e2260ab8caad47101063280b
Aniket <aniketkumarg99@gmail.com>
2024-07-02 18:58:56 +0530

    lang toggle

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middleware.WWWRedirectMiddleware', 
 ]
 
 ROOT_URLCONF = 'config.urls'
