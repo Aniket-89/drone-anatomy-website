e3a5075b3d42f59f218627a989f9873a08d2a23c
Aniket <aniketkumarg99@gmail.com>
2024-08-09 13:06:36 +0530

    --

--- gallery/models.py
+++ gallery/models.py
@@ -1,5 +1,8 @@
-from django.db import models
+import io
 from PIL import Image
+from django.db import models
+from django.core.files.base import ContentFile
+
 
 # Create your models here.
 class GalleryImage(models.Model):
@@ -19,21 +22,29 @@ class GalleryImage(models.Model):
             return None
         
     def save(self, *args, **kwargs):
+        # Save the instance first to ensure `self.image` has a value
         super(GalleryImage, self).save(*args, **kwargs)
 
         if self.image:
-            image = Image.open(self.image.path)
-
-            # Set the desired max width
+            # Open the image using the in-memory file
+            image = Image.open(self.image)
             max_width = 800
 
             # Resize the image if it's wider than the max width
             if image.width > max_width:
-                # Calculate the height using the same aspect ratio
                 aspect_ratio = image.height / image.width
                 new_height = int(max_width * aspect_ratio)
                 new_size = (max_width, new_height)
 
                 # Resize the image
                 image = image.resize(new_size, Image.Resampling.LANCZOS)
-                image.save(self.image.path, quality=80)
+
+                # Save the image to a BytesIO object
+                image_io = io.BytesIO()
+                image.save(image_io, format=image.format, quality=80)
+
+                # Save the new image to the ImageField
+                self.image.save(self.image.name, ContentFile(image_io.getvalue()), save=False)
+
+        # Call save again to save the resized image
+        super(GalleryImage, self).save(*args, **kwargs)
